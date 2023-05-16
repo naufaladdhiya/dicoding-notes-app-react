f980a79933d9570a5966dbde74fdcd53b7c9cf7d
naufaladdhiya <naufal.addhiya@gmail.com>
2023-05-17 00:31:22 +0700

    fix limit max title length

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,46 +1,49 @@
 import React, { Component } from 'react';
 
 class NoteInput extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         title: '',
-         body: '',
-      }
+  constructor(props) {
+    super(props);
 
-      this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this);
-      this.onBodyChangeHandler = this.onBodyChangeHandler.bind(this);
-      this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    }
+    this.state = {
+      title: '',
+      body: '',
+      limit: 50,
+    };
 
-    onTitleChangeHandler = (event) => {
-        this.setState(() => {
-            return {
-                title: event.target.value
-            }
-        })
-    }
+    this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this);
+    this.onBodyChangeHandler = this.onBodyChangeHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
 
-    onBodyChangeHandler = (event) => {
-        this.setState(() => {
-            return {
-                body: event.target.value
-            }
-        })
-    }
+  onTitleChangeHandler = (event) => {
+    this.setState(() => {
+      return {
+        title: event.target.value,
+      };
+    });
+  };
 
-    onSubmitHandler = (event) => {
-        event.preventDefault();
-        this.props.addNote(this.state)
-    }
-    
+  onBodyChangeHandler = (event) => {
+    this.setState(() => {
+      return {
+        body: event.target.value,
+      };
+    });
+  };
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+    this.props.addNote(this.state);
+  };
+
   render() {
     return (
       <div className="note-input">
         <h2>Buat Catatan</h2>
         <form onSubmit={this.onSubmitHandler}>
-          <p className="note-input__title__char-limit">Sisa karakter:</p>
+          <p className="note-input__title__char-limit">
+            Sisa karakter: {this.state.limit - this.state.title.length}
+          </p>
           <input
             type="text"
             placeholder="ini adalah judul ..."
